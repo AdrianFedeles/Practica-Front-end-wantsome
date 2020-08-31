dcec861ea9b93085c9af5e8ca250bd65c6d8d607
AdrianFedeles <65623604+AdrianFedeles@users.noreply.github.com>
2020-08-31 19:09:46 +0300

    Tema 6

--- tema 6/partea 2.js
+++ tema 6/partea 2.js
@@ -1,4 +1,11 @@
-//exercitiul 4
+//Scrieti o functie care accepta ca argument venitul anual al unui angajat in dolari (ex: $30,000) si calculeaza taxa
+//corespunzatoare considerand urmatoarele criterii:
+//- daca angajatul castiga anual o suma mai mica de $30,000, taxa este 0%
+//- daca venitul anual se situeaza intre $50,000 - $99,999, taxa calculata trebuie sa fie 35%
+//- pentru venit mai mare de $100,000, taxa se calculeaza ca procentaj de 40%
+//Returnati un text care sa aiba un format asemanator: “Pentru un venit anual de x, aveti de platit taxe in valoare de y”
+
+
 function taxe(salar) {
     var salarAnual = salar;
     if (salarAnual < 30.000) {
@@ -18,125 +25,93 @@ function taxe(salar) {
     }
 }
 console.log(taxe(76.569));
+
+
 //exeritiul 5
 function codifyPoints(points) {
     if (points < 30 || points == 30) {
         return 'FF';
-    } else if (40 < points && points < 50 || points == 50) {
+    }
+    else if
+        ((30 < points && points < 40) || points == 40) {
+        return "DD";
+    }
+    else if
+        (40 < points && points < 50 || points == 50) {
         return 'CD';
-    } else if (50 < points && points < 60 || points == 60) {
+    }
+    else if
+        (50 < points && points < 60 || points == 60) {
         return 'CC';
-    } else if (60 < points && points < 70 || points == 70) {
+    }
+    else if
+        (60 < points && points < 70 || points == 70) {
         return 'BC';
-    } else if (70 < points && points < 80 || points == 80) {
+    }
+    else if
+        (70 < points && points < 80 || points == 80) {
         return 'BB';
-    } else if (80 < points && points < 90 || points == 90) {
+    }
+    else if
+        (80 < points && points < 90 || points == 90) {
         return 'AB';
-    } else {
+    }
+    else {
         return 'AA';
     };
 };
 console.log(codifyPoints(20));
 
 
-// alta modalitate
+// alta modalitate 
 
-function points(codifyPoints) {
+function point(forPoints) {
     switch (true) {
-        case codifyPoints <= 30: {
-            return "DD";
-        }
-        case codifyPoints <= 40: {
-            return "FF";
-        }
-        case codifyPoints <= 50: {
-            return "EE";
-        }
-        case codifyPoints <= 60: {
-            return "FF";
-        }
-        case codifyPoints <= 70: {
-            return "DD";
-        }
-        case codifyPoints <= 80: {
-            return "HH";
-        }
-        case codifyPoints <= 90: {
-            return "JJ";
-        }
-        case point > 90: {
-            return "AA";
-        }
+        case forPoints <= 30:
+            {
+                return "FF";
+            }
+        case forPoints <= 40:
+            {
+                return "DD";
+            }
+        case forPoints <= 50:
+            {
+                return "CD";
+            }
+        case forPoints <= 60:
+            {
+                return "CC";
+            }
+        case forPoints <= 70:
+            {
+                return "BC";
+            }
+        case forPoints <= 80:
+            {
+                return "BB";
+            }
+        case forPoints <= 90:
+            {
+                return "AB";
+            }
+        case forPoints > 90:
+            {
+                return "AA";
+            }
     }
 }
 
-console.log(points(30))
+console.log(point(30))
 
 
 //exercitiul 6
 
-function is_weekend(date) {
-    switch (true) {
-        case date = 'Nov 13,2020': {
-            return 'nu e weekend';
-        }
-        case date = 'Nov 14,2020': {
-            return 'e weekend';
-        }
-        case date = 'Nov 15,2020': {
-            return 'e weekend';
-        }
-        case date = 'Nov 16,2020': {
-            return 'nu e weekend'
-        }
-        case date = 'Nov 17,2020': {
-            return 'nu e weekend';
-        }
-        case date = 'Nov 18,2020': {
-            return 'nu e weekend';
-        }
-        case date = 'Nov 19,2020': {
-            return 'nu e weekend';
-        }
-    }
-}
+//Nu stiu sa il fac.
+
+//Test Data :
 console.log(is_weekend('Nov 15, 2014'));
-
-function is_weekend(date) {
-    switch (true) {
-        case 1:
-            date == 'Nov 13,2020';
-            return 'nu e weekend';
-            break;
-        case 2:
-            date == 'Nov 14,2020';
-            return 'e weekend';
-            break;
-        case 3:
-            date == 'Nov 15,2020';
-            return 'e weekend';
-            break;
-        case 4:
-            date == 'Nov 16,2020';
-            return 'nu e weekend';
-            break;
-        case 5:
-            date == 'Nov 17,2020';
-            return 'nu e weekend';
-            break;
-        case 6:
-            date == 'Nov 18,2020';
-            return 'nu e weekend';
-            break;
-        case 7:
-            date == 'Nov 19,2020';
-            return 'nu e weekend';
-
-    }
-}
-console.log(is_weekend('Nov 15, 2014'));
-
-
-
+console.log(is_weekend('Nov 16, 2014'));
+console.log(is_weekend('Nov 17, 2014'));
 
 
--- tema 6/partea1.js
+++ tema 6/partea1.js
@@ -1,20 +1,37 @@
-//partea 1 din tema 1
-// 5
+//partea 1 din tema 6
+
+//Ex1: Creati un array care sa aiba length-ul 5. Demonstrati ca length-ul este 5.
+
+var arrLenght = [1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 2.5];
+console.log(arrLenght.length);
+
+//Ex5: Adaugati 2 elemente noi in arrayul de mai jos
+
 var cars = ['Tesla', 'Dacia'];
 cars.push("Tico", "Aro");
 console.log(cars);
-// 6
+
+//Ex6: Avand rezultatul de la exercitiul anterior, scoateti ultimul element intrat.
+
 cars.pop();
 console.log(cars);
-// 7
+
+//Ex7: Avand rezultatul exercitiului anterior, adaugati elementul 'Dodge' la inceputul arrayului
+
 cars.unshift("Dodge");
 console.log(cars);
-// 8
+
+//Ex8: Avand rezultatul exercitiului anterior, inversati ordinea elementelor din array
+
 cars.reverse();
 console.log(cars);
-// 9
+
+//Ex9: Avand in vedere rezultatul anterior Folositi o metoda pe arrayuri care sa inlocuiasca incepand de la elementul 'Tesla' si sa adauge alte 2 elemente noi - splice();
+
 cars.splice(2, 2, "Daf", "Kamaz");
 console.log(cars);
+
+//Ex10: Sortati urmatorul array de la mic la mare si de la mare la mic 
 // 10-a mic-mare
 var arrayNumbers = [1, 5, 20, 30, 100];
 arrayNumbers.sort()
@@ -30,7 +47,8 @@ arrayNumbers.sort(function (a, b) {
 });
 console.log(arrayNumbers);
 
-// 11
+//Ex11 : Aveti urmatorul array - folositi o functie care sa ia arrayul ca argument si sa-l returneze ordonat
+
 var fruitsArray = ["Apple", "Lemon", "Banana", "Orange", "Bere", "Mici"]
 fruitsArray.sort();
 console.log(fruitsArray);
@@ -42,8 +60,9 @@ function sort(sortFunction) {
 console.log(sort(fruitsArray));
 
 
-//partea 2 din tema 1
-//Exercitiul 4
+//partea 2 din tema 6
+////Ex4 
+// Creati un obiect cu cateva proprietati
 
 var agentie = {
     angajati: 3,
@@ -51,14 +70,20 @@ var agentie = {
     zone: "copou",
     sefAgentie: "Mihai",
 };
+
 //Copiere obiect
+
 var copieAgentie = Object.assign(agentie);
 console.log(copieAgentie);
-//adaugare
+
+//adaugare proprietati
+
 copieAgentie.varsteMax = 30;
 copieAgentie.masa = "calda";
 console.log(agentie)
+
 //obircte noi
+
 myHouse = {
     rooms: 3,
     zone: "Bucium",
@@ -66,6 +91,7 @@ myHouse = {
 }
 var copieMergeAgentie = Object.assign(copieAgentie, myHouse);
 console.log(copieMergeAgentie);
+
 //5
 //Creat obiect
 var combinaMuzicala = {
@@ -80,35 +106,57 @@ var combinaMuzicala = {
     transport: "da"
 }
 console.log(combinaMuzicala);
+
 //Key-variabila separata
+
 var objKeys = Object.keys(combinaMuzicala.audio);
 console.log(objKeys);
+
 //arrayul intr-un singur string 
+
 var strKey = objKeys.join(", ");
 console.log(strKey);
 console.log(strKey.slice(0, 3) + " si " + strKey.slice(6, strKey.lenght) + " apartin obiectului x");
-//6
+
+//Ex6
+// Avem obiectul :
+
 myObject = {
     name: "John",
     surname: "Applegate",
 }
+
+//adaugat metoda
+
 var sortNume = Object.values(myObject).sort();
 console.log(sortNume);
-//7
+
+//Ex7 
 //functia constructor
+
 function person(name, prename, id, acces) {
     this.name = name;
     this.prename = prename;
     this.id = id;
     this.acces = acces;
 };
+
 //3 valori diferite
-var newPerson1 = new person("Mihai", "Berbeceanu", 226, "total");
-var newPerson2 = new person("Marius", "Mihailescu", 333, "partial");
-var newPerson3 = new person("Nicolaie", "Fantu", 102, "partial");
+
+var newPerson1 = new person("Mihai", "Berbeceanu", 2262, "total");
+var newPerson2 = new person("Marius", "Mihailescu", 3337, "partial");
+var newPerson3 = new person("Nicolaie", "Fantu", 1021, "partial");
 console.log(newPerson1, newPerson2, newPerson3);
+
 //Uppercase
 
+function person(name, prename, id, acces) {
+    this.name = name.toUpperCase();
+    this.prename = prename.toUpperCase();
+    this.id = id;
+    this.acces = acces.toUpperCase();
+}
+console.log(newPerson1, newPerson2, newPerson3);
 
 
 
